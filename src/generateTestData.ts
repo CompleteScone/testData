cae3b223a88945b23cfbde40210ee8dd1c3b1c3f
George Swartzendruber <George.Swartzendruber@fhr.com>
2020-05-13 21:59:46 -0500

    actually update with people, after type change

--- src/generateTestData.ts
+++ src/generateTestData.ts
@@ -2,7 +2,7 @@ import { testData } from './testData';
 import * as fs from 'fs';
 
 const output = JSON.stringify(testData);
-fs.writeFile('../db.json', output, 'utf8', function (err) {
+fs.writeFile('db.json', output, 'utf8', function (err) {
     if (err) {
         console.log("An error occurred while writing JSON Object to File.");
         return console.log(err);
